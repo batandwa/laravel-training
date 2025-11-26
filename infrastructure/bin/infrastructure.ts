fe74362b84bc962878caff001751fdcb392e7c87
Batandwa Colani <Batandwa.Colani@multichoice.com>
2025-11-26 12:31:40 +0200

    Configure db networking and secrets

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', {
+new InfrastructureStack(app, 'infrastructure', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,6 +15,7 @@ new InfrastructureStack(app, 'InfrastructureStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: process.env.ACCOUNT_ID, region: process.env.AWS_REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
+import { Instance, InstanceClass, InstanceSize, InstanceType, Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
+import { Repository, TagMutability } from 'aws-cdk-lib/aws-ecr';
+import { ClusterInstance, DatabaseCluster, DatabaseClusterEngine, DatabaseInstance, DatabaseInstanceEngine, MariaDbEngineVersion, MysqlEngineVersion, SubnetGroup, Credentials } from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = Vpc.fromLookup(this, "VPC", {
+      vpcId: process.env.VPC_ID
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Repository(this, "Repo", {
+      repositoryName: "api",
+      imageTagMutability: TagMutability.IMMUTABLE,
+    });
+
+    const dbSubnetGroup = new SubnetGroup(this, 'DatabaseSubnetGroup', {
+      vpc,
+      vpcSubnets: {
+        subnetType: SubnetType.PUBLIC
+      },
+      description: 'Public RDS instance'
+    });
+
+    const cluster = new DatabaseInstance(this, 'Database', {
+      vpc,
+      engine: DatabaseInstanceEngine.mariaDb({
+        version: MariaDbEngineVersion.VER_10_11
+      }),
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
+      subnetGroup: dbSubnetGroup,
+      credentials: Credentials.fromGeneratedSecret('api'),
+    });
   }
 }
